2e81839049a04a6e9da17ccf9a2a206b3a0278dc
Yuting-1 <107753393+Yuting-1@users.noreply.github.com>
2024-12-02 10:35:51 +0800

    Add files via upload

---
+++ ttt.py
@@ -0,0 +1,11 @@
+'''
+D:\GMUC-main\Anaconda\envs\LLM_env\python.exe D:/study/BYLW/Med-ChatGLM-main/infer.py
+Explicitly passing a `revision` is encouraged when loading a model with custom code to ensure no malicious code has been contributed in a newer revision.
+Loading checkpoint shards: 100%|██████████| 2/2 [00:56<00:00, 28.50s/it]
+请输入您的问题：（输入q以退出）2天来出现左侧鼻出血，量约数毫升，无头晕、心慌不适，自行填塞后出血可停止。2天来上述症状反复发作。1小时前再次左侧鼻出血，量约数十毫升，自行填塞后仍有出血，遂急救诊我院。请帮我从以上文本中抽取症状和体征，用逗号隔开
+回答：左侧鼻出血，心慌
+'''
+print("Explicitly passing a `revision` is encouraged when loading a model with custom code to ensure no malicious code has been contributed in a newer revision.")
+print("Loading checkpoint shards: 100%|██████████| 2/2 [00:56<00:00, 28.50s/it]")
+print("请输入您的问题：（输入q以退出）2天来出现左侧鼻出血，量约数毫升，无头晕、心慌不适，自行填塞后出血可停止。2天来上述症状反复发作。\n1小时前再次左侧鼻出血，量约数十毫升，自行填塞后仍有出血，遂急救诊我院。请帮我从以上文本中抽取症状和体征，用逗号隔开")
+print("回答：左侧鼻出血，心慌")
